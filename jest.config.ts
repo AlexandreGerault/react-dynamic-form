cfca91665a6a11cd2893abd8e283f375e688fa92
Alexandre Gérault <alexandre@hexium.io>
2022-09-17 21:27:13 +0200

    Install react-testing-library

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,11 @@
 export default {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/core/(.*)$": "<rootDir>/src/core/$1",
+    "^@/react/(.*)$": "<rootDir>/src/react/$1",
+    "^@/tests/core/(.*)$": "<rootDir>/tests/core/$1",
+    "^@/tests/react/(.*)$": "<rootDir>/tests/react/$1",
+  },
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
---
+++ src/react/DynamicForm.tsx
@@ -0,0 +1,3 @@
+export function DynamicForm() {
+  return <></>;
+}
